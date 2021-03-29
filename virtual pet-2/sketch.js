2753250fc00f299e8de0f04e0fe06568c44ac679
shayonish <69904313+shayonish@users.noreply.github.com>
2021-03-29 18:08:16 +0530

    Add files via upload

--- virtual pet-2/sketch.js
+++ virtual pet-2/sketch.js
@@ -8,9 +8,6 @@ var gameState,readState;
 function preload(){
 sadDog=loadImage("Images/Dog.png");
 happyDog=loadImage("Images/happy dog.png");
-garden=loadImage("Images/Garden.png");
-washroom=loadImage("Images/Wash Room.png");
-bedroom=loadImage("Images/Bed Room.png");
 }
 
 function setup() {
@@ -19,20 +16,12 @@ function setup() {
   
   foodObj = new Food();
 
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
-
   fedTime=database.ref('FeedTime');
   fedTime.on("value",function(data){
     lastFed=data.val();
   });
 
-  //read game state from database
-  readState=database.ref('gameState');
-  readState.on("value",function(data){
-    gameState=data.val();
-  });
-   
+ 
   dog=createSprite(200,400,150,150);
   dog.addImage(sadDog);
   dog.scale=0.15;
@@ -47,38 +36,22 @@ function setup() {
 }
 
 function draw() {
-  currentTime=hour();
-  if(currentTime==(lastFed+1)){
-      update("Playing");
-      foodObj.garden();
-   }else if(currentTime==(lastFed+2)){
-    update("Sleeping");
-      foodObj.bedroom();
-   }else if(currentTime>(lastFed+2) && currentTime<=(lastFed+4)){
-    update("Bathing");
-      foodObj.washroom();
-   }else{
-    update("Hungry")
-    foodObj.display();
-   }
-   
-   if(gameState!="Hungry"){
-     feed.hide();
-     addFood.hide();
-     dog.remove();
-   }else{
-    feed.show();
-    addFood.show();
-    dog.addImage(sadDog);
-   }
- 
+  foodObj.display();
   drawSprites();
-}
-
-//function to read food Stock
-function readStock(data){
-  foodS=data.val();
-  foodObj.updateFoodStock(foodS);
+  fedTime = database.ref("fedtime");
+  fedTime.on("value",(data)=>{
+    lastFed = data.val})
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12)
+  {
+  text("lastFed : "+lastFed%12+"TM" , 350 , 30);
+  }
+  else
+  if(lastFed === 0)
+  {
+  text("lastFed : "+lastFed%12+"AM" , 350 , 30);
+  }
 }
 
 
@@ -99,12 +72,5 @@ function addFoods(){
   foodS++;
   database.ref('/').update({
     Food:foodS
-  })
-}
-
-//update gameState
-function update(state){
-  database.ref('/').update({
-    gameState:state
   })
 }
